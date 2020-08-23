dfd139a1de4b61ab2607131a0cd16990c96a9a70
ABC10946 <abctech10946@gmail.com>
2020-08-23 15:14:47 +0900

    エラーメッセージを改善

--- abcc.c
+++ abcc.c
@@ -5,6 +5,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+char *user_input;
+
+void error_at(char *loc, char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+
+	int pos = loc - user_input;
+	fprintf(stderr, "%s\n", user_input);
+	fprintf(stderr, "%*s", pos, "");
+	fprintf(stderr, "^ ");
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
 // トークンの種類を列挙型にする
 typedef enum {
 	TK_RESERVED, // 記号
@@ -49,14 +64,14 @@ bool consume(char op) {
 
 void expect(char op) {
 	if (token->kind != TK_RESERVED || token->str[0] != op)
-		error("'%c'ではありません", op);
+		error_at(token->str, "'%c'ではありません", op);
 
 	token = token->next;
 }
 
 int expect_number() {
 	if (token->kind != TK_NUM)
-		error("数ではありません");
+		error_at(token->str, "数ではありません");
 
 	int val = token->val;
 	token = token->next;
@@ -75,7 +90,8 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
 	return tok;
 }
 
-Token *tokenize(char *p) {
+Token *tokenize() {
+	char *p = user_input;
 	Token head;
 	head.next = NULL;
 	Token *cur = &head;
@@ -97,7 +113,7 @@ Token *tokenize(char *p) {
 			continue;
 		}
 
-		error("トークナイズできません");
+		error_at(token->str, "トークナイズできません");
 	}
 
 	new_token(TK_EOF, cur, p);
@@ -106,11 +122,12 @@ Token *tokenize(char *p) {
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
-		fprintf(stderr, "引数の個数が正しくありません\n");
+		error("引数の個数が正しくありません\n");
 		return 1;
 	}
 
-	token = tokenize(argv[1]);
+	user_input = argv[1];
+	token = tokenize();
 
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
