3d049778c19e1386c0c50ed475f0a060f42c9237
ABC10946 <abctech10946@gmail.com>
2022-07-27 22:02:17 +0900

    consumeを文字列に対応

--- abcc.c
+++ abcc.c
@@ -5,7 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef enum {
+typedef enum
+{
 	ND_ADD,
 	ND_SUB,
 	ND_MUL,
@@ -15,7 +16,8 @@ typedef enum {
 
 typedef struct Node Node;
 
-struct Node {
+struct Node
+{
 	NodeKind kind;
 	Node *lhs;
 	Node *rhs;
@@ -25,7 +27,7 @@ struct Node {
 char *user_input;
 
 // プロトタイプ宣言
-bool consume(char);
+bool consume(char *);
 void expect(char);
 int expect_number();
 bool at_eof();
@@ -36,8 +38,8 @@ Node *unary();
 Node *new_node_num(int val);
 Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
 
-
-void error_at(char *loc, char *fmt, ...) {
+void error_at(char *loc, char *fmt, ...)
+{
 	va_list ap;
 	va_start(ap, fmt);
 
@@ -51,23 +53,27 @@ void error_at(char *loc, char *fmt, ...) {
 }
 
 // トークンの種類を列挙型にする
-typedef enum {
+typedef enum
+{
 	TK_RESERVED, // 記号
-	TK_NUM,      // 整数
-	TK_EOF       // 終了
+	TK_NUM,		 // 整数
+	TK_EOF		 // 終了
 } TokenKind;
 
 typedef struct Token Token;
 
 // トークン構造体
-struct Token {
+struct Token
+{
 	TokenKind kind; // トークンの型 上で作った列挙型
-	Token *next;    // 次のトークンへのポインタ
-	int val;        // kindがTK_NUMのときの数値
-	char *str;      // トークン文字列
+	Token *next;	// 次のトークンへのポインタ
+	int val;		// kindがTK_NUMのときの数値
+	char *str;		// トークン文字列
+	int len;		// トークンの長さ
 };
 
-Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
 	Node *node = calloc(1, sizeof(Node));
 	node->kind = kind;
 	node->lhs = lhs;
@@ -75,66 +81,87 @@ Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
 	return node;
 }
 
-Node *new_node_num(int val) {
+Node *new_node_num(int val)
+{
 	Node *node = calloc(1, sizeof(Node));
 	node->kind = ND_NUM;
 	node->val = val;
 	return node;
 }
 
-Node *primary() {
-	if(consume('(')) {
+Node *primary()
+{
+	if (consume("("))
+	{
 		Node *node = expr();
 		expect(')');
 		return node;
 	}
 
-	return  new_node_num(expect_number());
+	return new_node_num(expect_number());
 }
 
 // mul = primary ( "*" primary| "/" primary)*
-Node *mul() {
+Node *mul() // トークンの長さ
+{
 	Node *node = unary();
 
-	for(;;) {
-		if(consume('*')) {
+	for (;;)
+	{
+		if (consume("*"))
+		{
 			node = new_node(ND_MUL, node, unary());
-		} else if(consume('/')) {
+		}
+		else if (consume("/"))
+		{
 			node = new_node(ND_DIV, node, unary());
-		} else {
+		}
+		else
+		{
 			return node;
 		}
 	}
 }
-
 
 // expr = mul ("+" mul | "-" mul)*
-Node *expr() {
+Node *expr()
+{
 	Node *node = mul();
-	for(;;) {
-		if(consume('+')) {
+	for (;;)
+	{
+		if (consume("+"))
+		{
 			node = new_node(ND_ADD, node, mul());
-		} else if(consume('-')) {
+		}
+		else if (consume("-"))
+		{
 			node = new_node(ND_SUB, node, mul());
-		} else {
+		}
+		else
+		{
 			return node;
 		}
 	}
 }
 
-Node *unary() {
-	if(consume('+')) {
+Node *unary()
+{
+	if (consume("+"))
+	{
 		return unary();
 	}
-	if(consume('-')) {
+	if (consume("-"))
+	{
 		return new_node(ND_SUB, new_node_num(0), unary());
 	}
 
 	return primary();
 }
 
-void gen(Node *node) {
-	if(node->kind == ND_NUM) {
+void gen(Node *node)
+{
+	if (node->kind == ND_NUM)
+	{
 		printf("	push %d\n", node->val);
 		return;
 	}
@@ -145,20 +172,21 @@ void gen(Node *node) {
 	printf("	pop rdi\n");
 	printf("	pop rax\n");
 
-	switch(node->kind){
-		case ND_ADD:
-			printf("	add rax, rdi\n");
-			break;
-		case ND_SUB:
-			printf("	sub rax, rdi\n");
-			break;
-		case ND_MUL:
-			printf("	imul rax, rdi\n");
-			break;
-		case ND_DIV:
-			printf("	cqo\n");
-			printf("	idiv rdi\n");
-			break;
+	switch (node->kind)
+	{
+	case ND_ADD:
+		printf("	add rax, rdi\n");
+		break;
+	case ND_SUB:
+		printf("	sub rax, rdi\n");
+		break;
+	case ND_MUL:
+		printf("	imul rax, rdi\n");
+		break;
+	case ND_DIV:
+		printf("	cqo\n");
+		printf("	idiv rdi\n");
+		break;
 	}
 
 	printf("	push rax\n");
@@ -167,8 +195,8 @@ void gen(Node *node) {
 // 今見てるトークン
 Token *token;
 
-
-void error(char *fmt, ...) {
+void error(char *fmt, ...)
+{
 	va_list ap;
 	va_start(ap, fmt);
 	vfprintf(stderr, fmt, ap);
@@ -181,22 +209,25 @@ consume, expect, expect_number, at_eof
 これらはトークンナイズ後に使われる関数
 */
 
-bool consume(char op) {
-	if (token->kind != TK_RESERVED || token->str[0] != op)
+bool consume(char *op)
+{
+	if (token->kind != TK_RESERVED || strlen(op) != token->len || memcmp(token->str, op, token->len)) // トークンの種類が記号でない & opの長さがトークンの長さと一致しない & トークン文字列とop文字列がtoken->len個の範囲で一致しているか確認(memcmpは一致している場合0を返す)
 		return false;
 
 	token = token->next;
 	return true;
 }
 
-void expect(char op) {
+void expect(char op)
+{
 	if (token->kind != TK_RESERVED || token->str[0] != op)
 		error_at(token->str, "'%c'ではありません", op);
 
 	token = token->next;
 }
 
-int expect_number() {
+int expect_number()
+{
 	if (token->kind != TK_NUM)
 		error_at(token->str, "数ではありません");
 
@@ -205,11 +236,13 @@ int expect_number() {
 	return val;
 }
 
-bool at_eof() {
+bool at_eof()
+{
 	return token->kind == TK_EOF;
 }
 
-Token *new_token(TokenKind kind, Token *cur, char *str) {
+Token *new_token(TokenKind kind, Token *cur, char *str)
+{
 	Token *tok = calloc(1, sizeof(Token));
 	tok->kind = kind;
 	tok->str = str;
@@ -217,25 +250,31 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
 	return tok;
 }
 
-Token *tokenize(char *p) {
+Token *tokenize(char *p)
+{
 	Token head;
 	head.next = NULL;
 	Token *cur = &head;
 
-	while (*p) {
-		if (isspace(*p)) {
+	while (*p)
+	{
+		if (isspace(*p))
+		{
 			p++;
 			continue;
 		}
 
-		if (strchr("+-*/()", *p)) {
+		if (strchr("+-*/()", *p))
+		{
 			cur = new_token(TK_RESERVED, cur, p++);
+			cur->len = 1;
 			continue;
 		}
 
-		if (isdigit(*p)) {
+		if (isdigit(*p))
+		{
 			cur = new_token(TK_NUM, cur, p);
-			cur->val = strtol(p, &p, 10);
+			cur->val = strtol(p, &p, 10); // 文字列をlong値に変換 (今回の場合10進数) 変換不可能な文字までのポインタを第2引数のendptrに格納する。これにより複数桁数字をvalに格納する。
 			continue;
 		}
 
@@ -246,8 +285,10 @@ Token *tokenize(char *p) {
 	return head.next;
 }
 
-int main(int argc, char **argv) {
-	if (argc != 2) {
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
 		error("引数の個数が正しくありません\n");
 		return 1;
 	}
